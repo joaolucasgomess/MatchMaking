4e2b7711263f5d08813b68c8e08377306193308d
JOAO LUCAS GOMES NAPOLES <1-22-11755@aluno.faminas.edu.br>
2023-04-24 22:32:05 -0300

    commit

--- Macthmaking.java
+++ Macthmaking.java
@@ -65,7 +65,6 @@ public class Macthmaking extends Uteis{
                   if(testarRole(inicio, fim) == true){
                      naoEntraPartida = false;
                      printar("deu certo");
-                     iniciarPartida();
                   }
                   fim++;
                }
@@ -75,10 +74,6 @@ public class Macthmaking extends Uteis{
       }else{
          printar("Nao ha jogadores o suficiente.");
       }
-   }
-   
-   public void iniciarPartida(){
-      //TODO
    }
    
    public boolean testarNivel(int inicio, int fim){
@@ -94,21 +89,236 @@ public class Macthmaking extends Uteis{
    }
    
    public boolean testarRole(int inicio, int fim){
+      //0 - carregador, 1 - tanker, 2 - suporte, 3 - mago
       Jogador[] jogadoresTestados = transicaoListaVetor(inicio, fim);
-      int[][] rolesContadas = contaRoles(jogadoresTestados);
-      int contaNulls = 0;
+      Jogador[] carregador = new Jogador[2];
+      Jogador[] tanker = new Jogador[2];
+      Jogador[] suporte = new Jogador[2];
+      Jogador[] mago = new Jogador[2];
       
-      for(int[] role : rolesContadas){
-         for(int id : role){
-            if(id == 0){
-               contaNulls++;
+      for(Jogador jogadorTestado : jogadoresTestados){
+          if(jogadorTestado.getRole() == 0){
+            if(carregador[0] == null){
+               carregador[0] = jogadorTestado;
+            }else{
+               carregador[1] = jogadorTestado;
+            }
+         }else if(jogadorTestado.getRole() == 1){
+           if(tanker[0] == null){
+               tanker[0] = jogadorTestado;
+            }else{
+               tanker[1] = jogadorTestado;
+            }
+         }else if(jogadorTestado.getRole() == 2){
+           if(suporte[0] == null){
+               suporte[0] = jogadorTestado;
+            }else{
+               suporte[1] = jogadorTestado;
+            }
+         }else{
+           if(mago[0] == null){
+               mago[0] = jogadorTestado;
+            }else{
+               mago[1] = jogadorTestado;
+            }
+        }
+      }
+      if(testarNull(carregador, tanker, suporte, mago) == true){
+         
+         return true;
+      }else{
+         return false;
+      }
+   }
+   
+   public void iniciarPartida(Jogador[] carregadores, Jogador[] tankers, Jogador[] suportes, Jogador[] magos){
+      Jogador[] equipe1 = new Jogador[3];
+      Jogador[] equipe2 = new Jogador[3];
+      int indice = 0;
+      for(Jogador integrante : equipe1){
+         if(integrante == null){
+            if(equipe1[2] != null){
+               break;
+            }
+            for(Jogador carregador : carregadores){
+               if(carregador != null){
+                  if(!existeARole(equipe1, 0)){
+                     equipe1[indice] = carregador;
+                     if(carregador[0] != null){
+                        carregador[0] = null;
+                     }else if(carregador[1] != null){
+                        carregador[1] = null;
+                     }
+                     indice++;
+                  }
+               }
+            }
+            if(equipe1[2] != null){
+               break;
+            }
+            for(Jogador tanker : tankers){
+               if(tanker != null){
+                  if(!existeARole(equipe1, 1)){
+                     equipe1[indice] = tanker;
+                     indice++;
+                     if(tanker[0] != null){
+                        tanker[0] = null;
+                     }else if(tanker[1] != null){
+                        tanker[1] = null;
+                     }
+                  }
+               }
+            }
+            if(equipe1[2] != null){
+               break;
+            }
+            for(Jogador suporte : suportes){
+               if(suporte != null){
+                  if(!existeARole(equipe1, 2)){
+                     equipe1[indice] = suporte;
+                     indice++;
+                     if(suporte[0] != null){
+                        suporte[0] = null;
+                     }else if(suporte[1] != null){
+                        suporte[1] = null;
+                     }
+                  }
+               }
+            }
+            if(equipe1[2] != null){
+               break;
+            }
+            for(Jogador mago : magos){
+               if(mago != null){
+                  if(!existeARole(equipe1, 3)){
+                     equipe1[indice] = mago;
+                     indice++;
+                     if(mago[0] != null){
+                        mago[0] = null;
+                     }else if(mago[1] != null){
+                        mago[1] = null;
+                     }
+                  }
+               }
             }
          }
       }
-      if(contaNulls > 2){
+      for(Jogador integrante : equipe2){
+         if(integrante == null){
+            if(equipe2[2] != null){
+               break;
+            }
+            for(Jogador carregador : carregadores){
+               if(carregador != null){
+                  if(!existeARole(equipe2, 0)){
+                     equipe2[indice] = carregador;
+                     if(carregador[0] != null){
+                        carregador[0] = null;
+                     }else if(carregador[1] != null){
+                        carregador[1] = null;
+                     }
+                     indice++;
+                  }
+               }
+            }
+            if(equipe2[2] != null){
+               break;
+            }
+            for(Jogador tanker : tankers){
+               if(tanker != null){
+                  if(!existeARole(equipe2, 1)){
+                     equipe2[indice] = tanker;
+                     indice++;
+                     if(tanker[0] != null){
+                        tanker[0] = null;
+                     }else if(tanker[1] != null){
+                        tanker[1] = null;
+                     }
+                  }
+               }
+            }
+            if(equipe2[2] != null){
+               break;
+            }
+            for(Jogador suporte : suportes){
+               if(suporte != null){
+                  if(!existeARole(equipe2, 2)){
+                     equipe2[indice] = suporte;
+                     indice++;
+                     if(suporte[0] != null){
+                        suporte[0] = null;
+                     }else if(suporte[1] != null){
+                        suporte[1] = null;
+                     }
+                  }
+               }
+            }
+            if(equipe2[2] != null){
+               break;
+            }
+            for(Jogador mago : magos){
+               if(mago != null){
+                  if(!existeARole(equipe2, 3)){
+                     equipe2[indice] = mago;
+                     indice++;
+                     if(mago[0] != null){
+                        mago[0] = null;
+                     }else if(mago[1] != null){
+                        mago[1] = null;
+                     }
+                  }
+               }
+            }
+         }
+      }  
+   }
+   
+   public boolean existeNoArray(Jogador[] arrayTeste, Jogador testado){
+      boolean existeNoArray = false;
+      for(Jogador jogador : arrayTeste){
+         if(jogador == null){
+            break;
+         }
+         else if(jogador.equals(testado)){
+            existeNoArray =  true;
+         }
+      }return existeNoArray;
+   }
+   
+   public boolean existeARole(Jogador[] arrayTeste, int role){
+      boolean existeARole = false;
+      for(Jogador jogador : arrayTeste){
+         if(jogador == null){
+            break;
+         }
+         else if(jogador.getRole() == role){
+            existeARole = true;
+         }
+      }return existeARole;
+   }
+   
+   public boolean testarNull(Jogador[] carregador, Jogador[] tanker, Jogador[] suporte, Jogador[] mago){
+      int count = 0; 
+
+      if (carregador.length == 0) {
+         count++; 
+      }
+      if (tanker.length == 0) {
+         count++; 
+      }
+      if (suporte.length == 0) {
+         count++; 
+      }
+      if (mago.length == 0) {
+         count++; 
+      }
+
+      if (count == 1 || count == 0) {
+         iniciarPartida(carregador, tanker, suporte, mago);
+         return true;
+      } else{
          return false;
       }
-      return true;
    }
    
    public Jogador[] transicaoListaVetor(int inicio, int fim){
@@ -120,24 +330,5 @@ public class Macthmaking extends Uteis{
          indice++;
       }
       return jogadoresTestados;
-   }
-   
-   public int[][] contaRoles(Jogador[] jogadoresTestados){
-      //0 - carregador, 1 - tanker, 2 - suporte, 3 - mago
-      int[][] ocorrenciasDeRoles = new int[4][2];
-      int indice = 0;
-      
-      while(indice < 4){
-         for(Jogador jogador : jogadoresTestados){
-            if(jogador.getRole() == indice){
-               if(ocorrenciasDeRoles[indice][0] == 0){
-                  ocorrenciasDeRoles[indice][0] = jogador.getId();
-               }else{
-                  ocorrenciasDeRoles[indice][1] = jogador.getId();
-               }
-            }
-         }indice++;
-      }
-      return ocorrenciasDeRoles;
    }
 }
